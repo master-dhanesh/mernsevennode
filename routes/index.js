3715ad351e7dd607603efa875a4eb20751882abb
master-dhanesh <dhanesh1296@gmail.com>
2023-09-12 10:23:06 +0530

    req.body and partials

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,15 @@ router.get("/user/:id", function (req, res, next) {
     res.render("users", { id: req.params.id });
 });
 
-router.get("/register", function (req, res, next) {
-    // res.send("text")
-    res.json(req.query);
+// router.get("/register", function (req, res, next) {
+//     // res.send("text")
+//     // res.json(req.query);
+//     res.render("Profile", { user: req.query });
+// });
+
+router.post("/register", function (req, res, next) {
+    const data = req.body;
+    res.render("Profile", { user: data });
 });
 
 module.exports = router;
